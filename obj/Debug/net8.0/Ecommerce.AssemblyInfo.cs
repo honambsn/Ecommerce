fe53ea31729bcf02ab357bb73ae32330a502f92e
honam <75104291+honambsn@users.noreply.github.com>
2024-05-08 00:12:23 +0700

    edit key

--- obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3fb7fe877a760bc887b5d62bc77b699236448b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e9fd2ea7093cd81464a7ad4b466fe4f45d068b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
